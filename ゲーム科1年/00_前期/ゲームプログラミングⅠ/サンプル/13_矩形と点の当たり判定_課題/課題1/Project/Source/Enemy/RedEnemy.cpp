0ec53d25edcec425b7529532020ce12194468e64
ArakiYusuke-Ohara <yusuke.araki@mail.o-hara.ac.jp>
2025-01-06 14:17:08 +0900

    サンプル作り忘れてたので作った

--- ゲーム科1年/00_前期/ゲームプログラミングⅠ/サンプル/13_矩形と点の当たり判定_課題/課題1/Project/Source/Enemy/RedEnemy.cpp
+++ ゲーム科1年/00_前期/ゲームプログラミングⅠ/サンプル/13_矩形と点の当たり判定_課題/課題1/Project/Source/Enemy/RedEnemy.cpp
@@ -2,27 +2,51 @@
 #include "RedEnemy.h"
 
 // •Ï”
-RedEnemyData g_RedEnemyData = { 0 };
+RedEnemyData g_RedEnemyData[RED_ENEMY_MAX] = {0};
 
 void InitRedEnemy()
 {
-	g_RedEnemyData.handle = LoadGraph("Data/Enemy/RedEnemy.png");
+	for (int i = 0; i < RED_ENEMY_MAX; i++)
+	{
+		g_RedEnemyData[i].handle = LoadGraph("Data/Enemy/RedEnemy.png");
 
-	g_RedEnemyData.posX = 1200.0f;
-	g_RedEnemyData.posY = 200.0f;
-	g_RedEnemyData.moveX = 0.0f;
-	g_RedEnemyData.moveY = 0.0f;
+		g_RedEnemyData[i].posX = 1200.0f - i * 300.0f;
+		g_RedEnemyData[i].posY = 200.0f + i * 200.0f;
+		g_RedEnemyData[i].moveX = 0.0f;
+		g_RedEnemyData[i].moveY = 0.0f;
+	}
 }
 
 void UpdateRedEnemy()
 {
+	for (int i = 0; i < RED_ENEMY_MAX; i++)
+	{
+		g_RedEnemyData[i].hitFlag = false;
+	}
 }
 
 void DrawRedEnemy()
 {
-	DrawGraph((int)g_RedEnemyData.posX, (int)g_RedEnemyData.posY, g_RedEnemyData.handle, TRUE);
+	for (int i = 0; i < RED_ENEMY_MAX; i++)
+	{
+		DrawGraph((int)g_RedEnemyData[i].posX, (int)g_RedEnemyData[i].posY, g_RedEnemyData[i].handle, TRUE);
+
+		if (g_RedEnemyData[i].hitFlag)
+		{
+			DrawFormatString(0, 40, GetColor(255, 255, 255), "RedEnemy[%d]:ƒ}ƒEƒX‚ª“–‚½‚Á‚½", i);
+		}
+	}
 }
 
 void FinRedEnemy()
 {
+	for (int i = 0; i < RED_ENEMY_MAX; i++)
+	{
+		DeleteGraph(g_RedEnemyData[i].handle);
+	}
+}
+
+RedEnemyData* GetRedEnemy()
+{
+	return g_RedEnemyData;
 }
