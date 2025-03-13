caf2ef1abd81b935f2b0eb2f17207ffcca3c76aa
ArakiYusuke-Ohara <yusuke.araki@mail.o-hara.ac.jp>
2025-03-13 13:23:27 +0900

    ファイル削除バッチ追加

---
+++ VSWorkdataDelete.bat
@@ -0,0 +1,26 @@
+@echo off
+chcp 65001 >nul
+set /p "target_dir=削除したいディレクトリのパスを入力してください: "
+
+if not exist "%target_dir%" (
+    echo 指定したパスが存在しません。
+    pause
+    exit /b
+)
+
+for /d /r "%target_dir%" %%d in (.vs) do (
+    if exist "%%d" (
+        rd /s /q "%%d"
+        echo "%%d フォルダを削除しました。"
+    )
+)
+
+for /d /r "%target_dir%" %%d in (x64) do (
+    if exist "%%d" (
+        rd /s /q "%%d"
+        echo "%%d フォルダを削除しました。"
+    )
+)
+
+echo "完了しました。"
+pause
