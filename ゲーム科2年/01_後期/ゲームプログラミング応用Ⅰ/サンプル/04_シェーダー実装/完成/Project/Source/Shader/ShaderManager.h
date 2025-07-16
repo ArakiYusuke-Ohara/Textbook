c60ae48181048d37e26dd7c11c0ed26ca92cb124
YusukeAraki <yusuke.araki@mail.o-hara.ac.jp>
2025-07-16 14:53:38 +0900

    シェーダー実装まで作成

---
+++ ゲーム科2年/01_後期/ゲームプログラミング応用Ⅰ/サンプル/04_シェーダー実装/完成/Project/Source/Shader/ShaderManager.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "../Singleton/Singleton.h"
+
+enum ShaderID
+{
+	NORMALMESH_NOLIGHT,
+	SHADER_ID_MAX
+};
+
+class ShaderManager : public Singleton<ShaderManager>
+{
+public:
+	~ShaderManager();
+
+public:
+	void Load();
+	void SetShader(int id);
+	void SetVertexShader(int id);
+	void SetPixelShader(int id);
+	void Fin();
+
+private:
+	// ロードした頂点シェーダー配列
+	int m_VSHandles[SHADER_ID_MAX] = {};
+	// ロードしたピクセルシェーダー配列
+	int m_PSHandles[SHADER_ID_MAX] = {};
+};
+
