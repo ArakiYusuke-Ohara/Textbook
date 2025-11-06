8d87dd6d9e54067a526d51997453690ee179c221
YusukeAraki <yusuke.araki@mail.o-hara.ac.jp>
2025-11-06 10:11:14 +0900

    プレイヤー同期サンプル作成

---
+++ ゲーム科3年/00_前期/オンラインゲームプログラミングⅠ/サンプル/09_プレイヤー同期/完成/Server/Source/Network/Server.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <list>
+#include <string>
+#include "NetworkCommonParam.h"
+
+
+struct ClientData
+{
+	int handle;
+	IPDATA ip;
+};
+
+struct NetworkPlayerData
+{
+	ClientData client;
+	int id;
+	VECTOR pos;
+	VECTOR scale;
+	VECTOR rot;
+};
+
+class Server
+{
+public:
+	Server();
+	~Server();
+
+public:
+	void Init();
+	void Update();
+	void Draw();
+	void Fin();
+
+private:
+	void AddUserData(int handle);		// ユーザーデータを追加
+	void RemoveUserData(int handle);	// ユーザーデータを除外
+	bool ReceiveData();					// データ受信
+	void SendLoginData(const NetworkPlayerData& loginPlayer);
+	void SendJoinData(const NetworkPlayerData& joinPlayer);
+	void SendLogoutData(int id);
+	void SendAllTransformData();
+	void SyncPos(int handle);
+	void SyncRot(int handle);
+	void SyncScale(int handle);
+	void SyncTransform(int handle);
+
+private:
+	std::list<NetworkPlayerData> m_NetworkPlayerData;
+};
